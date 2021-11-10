61567ffe8c29e22cb5182e84a2761cd4a06d58f3
tiryakiv <48734237+tiryakiv@users.noreply.github.com>
2021-11-10 23:59:15 +0300

    Add files via upload

---
+++ valid/predh/eval_performance.m
@@ -0,0 +1,28 @@
+clear;clc;close all; format compact
+
+vid='v001'; %Model version
+
+for i1=1:8  % There are 8 test images
+ fileNameGT=strcat(num2str(i1-1),'_gt.png');
+ fileNamePredict=strcat(num2str(i1-1),'_predict_',vid,'.png');
+ label=imread(fileNameGT); 
+ pred=imread(fileNamePredict);
+ [acc(i1),cellAcc(i1),cultAcc(i1),iou_cel(i1),iou_cul(i1),mcc(i1), ...
+ prec(i1),recall(i1), dsc(i1), dc_cul(i1), dc_cel(i1)]=metrics(label,pred);
+end
+
+mAcc=mean(acc)
+mcellAcc=mean(cellAcc);
+mcultAcc=mean(cultAcc);
+
+mMCC=mean(mcc)
+mPrec=mean(prec)
+mRecall=mean(recall)
+
+mDc=mean(dsc);
+mDcCult=mean(dc_cul);
+mDSCCell=mean(dc_cel)
+mIoUCell=mean(iou_cel);
+mIoUCult=mean(iou_cul);
+
+
